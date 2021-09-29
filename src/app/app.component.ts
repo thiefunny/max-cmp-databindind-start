448539af09df1cfcc47b6473173348de99ff840c
thiefunny <mikolaj@walanus.com>
2021-09-29 09:25:17 +0200

    initianl commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  serverElements = [{type: 'server', name: 'testServer', content: 'Just a Test!'},{type: 'server', name: 'testServer', content: 'Just a 2Test!'}];
+  
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+    console.log(blueprintData)
+    console.log(this.serverElements)
+  }
+
+}
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,49 @@
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output
+} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  @Output() serverCreated = new EventEmitter < {
+    serverName: string,
+    serverContent: string
+  } > ();
+  
+  @Output('bpCreated') blueprintCreated = new EventEmitter < {
+    serverName: string,
+    serverContent: string
+  } > ()
+
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer(nameInput: HTMLInputElement) {
+    // console.log(nameInput.value)
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.newServerContent
+    });
+  }
+
+
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
